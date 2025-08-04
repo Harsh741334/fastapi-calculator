92835689f36e16d51cb1df7e0d29e89decbfa663
Harsh Agarwal <your.email@example.com>
2025-08-04 17:22:03 +0530

    Final Changes

--- term02.py
+++ term02.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import HTMLResponse
 from pydantic import BaseModel
 
 app = FastAPI()
@@ -12,6 +13,12 @@ app.add_middleware(
     allow_methods=["*"],  # Allows all methods
     allow_headers=["*"],  # Allows all headers
 )
+
+@app.get("/", response_class=HTMLResponse)
+async def get_frontend():
+    with open("index.html", "r", encoding="utf-8") as file:
+        html_content = file.read()
+    return HTMLResponse(content=html_content, status_code=200)
 
 class Input(BaseModel):
     a: int
